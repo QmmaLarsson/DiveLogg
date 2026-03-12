068901d2a09990cbdc24b0dd20a372c2f43b21e8
Emma Larsson <emma940112@gmail.com>
2026-03-12 11:13:41 +0100

    Har lagt till så att man kan filtrera dykloggen på grupp och person, funktionalitet skapades i DiveController och selectboxar i Index.cshtml (Dive)

--- Migrations/DiveLoggContextModelSnapshot.cs
+++ Migrations/DiveLoggContextModelSnapshot.cs
@@ -324,9 +324,11 @@ namespace DiveLogg.Migrations
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityUserLogin<string>", b =>
                 {
                     b.Property<string>("LoginProvider")
+                        .HasMaxLength(128)
                         .HasColumnType("TEXT");
 
                     b.Property<string>("ProviderKey")
+                        .HasMaxLength(128)
                         .HasColumnType("TEXT");
 
                     b.Property<string>("ProviderDisplayName")
@@ -364,9 +366,11 @@ namespace DiveLogg.Migrations
                         .HasColumnType("TEXT");
 
                     b.Property<string>("LoginProvider")
+                        .HasMaxLength(128)
                         .HasColumnType("TEXT");
 
                     b.Property<string>("Name")
+                        .HasMaxLength(128)
                         .HasColumnType("TEXT");
 
                     b.Property<string>("Value")
